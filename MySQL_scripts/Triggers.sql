d77cff7723e686255435f6d2953c1158ee66fc04
mapachass16 <42397265+mapachass16@users.noreply.github.com>
2019-11-03 22:45:32 -0600

    pending triggers

--- MySQL_scripts/Triggers.sql
+++ MySQL_scripts/Triggers.sql
@@ -204,3 +204,139 @@ BEGIN
     SET NEW.last_modification_date := SYSDATE();
 END;
 $$
+###################################################################
+-- Social_Event
+CREATE DEFINER= root@localhost TRIGGER Social_Event_BEFORE_INSERT 
+BEFORE INSERT ON social_event FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER Social_Event_BEFORE_UPDATE
+BEFORE UPDATE ON social_event FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
+###################################################################
+-- Address
+CREATE DEFINER= root@localhost TRIGGER Address_BEFORE_INSERT 
+BEFORE INSERT ON address FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER Address_BEFORE_UPDATE
+BEFORE UPDATE ON address FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
+###################################################################
+-- District
+CREATE DEFINER= root@localhost TRIGGER District_BEFORE_INSERT 
+BEFORE INSERT ON district FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER District_BEFORE_UPDATE
+BEFORE UPDATE ON district FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
+###################################################################
+-- Canton
+CREATE DEFINER= root@localhost TRIGGER Canton_BEFORE_INSERT 
+BEFORE INSERT ON canton FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER Canton_BEFORE_UPDATE
+BEFORE UPDATE ON canton FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
+###################################################################
+-- Province
+CREATE DEFINER= root@localhost TRIGGER Province_BEFORE_INSERT 
+BEFORE INSERT ON province FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER Province_BEFORE_UPDATE
+BEFORE UPDATE ON province FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
+###################################################################
+-- Country
+CREATE DEFINER= root@localhost TRIGGER Country_BEFORE_INSERT 
+BEFORE INSERT ON country FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER Country_BEFORE_UPDATE
+BEFORE UPDATE ON country FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
+###################################################################
+-- Parameter
+CREATE DEFINER= root@localhost TRIGGER Parameter_BEFORE_INSERT 
+BEFORE INSERT ON parameter FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER Parameter_BEFORE_UPDATE
+BEFORE UPDATE ON parameter FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
+###################################################################
+-- Event_Log
+CREATE DEFINER= root@localhost TRIGGER Event_Log_BEFORE_INSERT 
+BEFORE INSERT ON event_log FOR EACH ROW 
+BEGIN
+	SET NEW.created_by := USER();
+    SET NEW.creation_date := SYSDATE();
+END;
+$$
+
+CREATE DEFINER= root@localhost TRIGGER Event_Log_BEFORE_UPDATE
+BEFORE UPDATE ON event_log FOR EACH ROW
+BEGIN
+	SET NEW.last_modified_by := USER();
+    SET NEW.last_modification_date := SYSDATE();
+END;
+$$
