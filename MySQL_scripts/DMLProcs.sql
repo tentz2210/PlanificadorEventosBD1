fc977645d93a5d30d5c015bf2bf0491fefb99354
lindsaymorales <41713266+lindsaymorales@users.noreply.github.com>
2019-11-09 12:53:21 -0600

    fixed some mistakes, added social event insertion and delete proc

--- MySQL_scripts/DMLProcs.sql
+++ MySQL_scripts/DMLProcs.sql
@@ -244,7 +244,7 @@ END;$$
 
 /* Country */
 -- INSERT
-CREATE PROCEDURE createCountry(IN p_country_name VARCHAR(35))
+CREATE PROCEDURE createCountry(IN p_country_name VARCHAR(30))
 BEGIN
 	DECLARE v_country_id int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -276,7 +276,7 @@ END;$$
 
 /* Province */
 -- INSERT
-CREATE PROCEDURE createProvince(IN p_province_name VARCHAR(35), IN p_country_id int)
+CREATE PROCEDURE createProvince(IN p_province_name VARCHAR(30), IN p_country_id int)
 BEGIN
 	DECLARE v_province_id int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -308,7 +308,7 @@ END;$$
 
 /* Canton */
 -- INSERT
-CREATE PROCEDURE createCanton(IN p_canton_name VARCHAR(35), IN p_province_id int)
+CREATE PROCEDURE createCanton(IN p_canton_name VARCHAR(30), IN p_province_id int)
 BEGIN
 	DECLARE v_canton_id int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -340,7 +340,7 @@ END;$$
 
 /* District */
 -- INSERT
-CREATE PROCEDURE createDistrict(IN p_district_name VARCHAR(35), IN p_canton_id int)
+CREATE PROCEDURE createDistrict(IN p_district_name VARCHAR(30), IN p_canton_id int)
 BEGIN
 	DECLARE v_district_id int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -372,7 +372,7 @@ END;$$
 
 /* Adress */
 -- INSERT
-CREATE PROCEDURE createAdress(IN p_address_desc VARCHAR(200), IN p_district_id int)
+CREATE PROCEDURE createAdress(IN p_address_desc VARCHAR(100), IN p_district_id int)
 BEGIN
 	DECLARE v_address_id int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -404,7 +404,7 @@ END;$$
 
 /* User Type */
 -- INSERT
-CREATE PROCEDURE createUserType(IN p_user_type_name VARCHAR(35))
+CREATE PROCEDURE createUserType(IN p_user_type_name VARCHAR(30))
 BEGIN
 	DECLARE v_user_type_id int;
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -430,6 +430,44 @@ BEGIN
     
     DELETE FROM user_type
     WHERE user_type_id = p_user_type_id;
+	COMMIT;
+    
+END;$$
+
+/* Social Event */
+-- INSERT
+CREATE PROCEDURE createSocialEvent(IN p_person_id int, IN p_address_id int, IN p_category_id int, IN p_event_tile VARCHAR(50),
+								   IN p_event_description VARCHAR(400), IN p_start_date VARCHAR(20), IN p_end_date VARCHAR(20),
+                                   IN p_is_private tinyint, IN p_photo VARCHAR(30))
+BEGIN
+	DECLARE v_event_id int;
+    DECLARE v_start_date DATE;
+    DECLARE v_end_date DATE;
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error inserting event';
+    END;
+    
+    SET v_event_id = LAST_INSERT_ID();
+    SET v_start_date := STR_TO_DATE(p_start_date, '%d/%m/%Y %H:%i');
+    SET v_end_date := STR_TO_DATE(p_end_date, '%d/%m/%Y %H:%i');
+    INSERT INTO social_event(event_id,person_id,address_id,category_id,event_title,event_description,start_date,end_date,isPrivate,photo)
+    VALUES (v_event_id,p_person_id,p_address_id,p_category_id,p_event_title,p_event_description,v_start_date,v_end_date,p_is_private,p_photo);
+    COMMIT;
+END;$$
+
+-- DELETE
+CREATE PROCEDURE deleteSocialEvent(IN p_social_event_id int)
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error deleting social event';
+    END;
+    
+    DELETE FROM event
+    WHERE event_id = p_social_event_id;
 	COMMIT;
     
 END;$$
