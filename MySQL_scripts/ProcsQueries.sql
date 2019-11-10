193397e7271bd7267e873af9d8d2093f01d1ba45
tentz2210 <tentz2210@hotmail.com>
2019-11-10 06:59:24 -0600

    added finished procedures for requested queries

--- MySQL_scripts/ProcsQueries.sql
+++ MySQL_scripts/ProcsQueries.sql
@@ -4,9 +4,13 @@
 DELIMITER $$
 /* ADMINS */
 -- Point a
-DROP PROCEDURE getInvitedUsers;$$
 CREATE PROCEDURE getInvitedUsers(IN filter_status_id int, IN filter_owner_id int)
 BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error executing query';
+    END;
 	SELECT CONCAT(per.first_name,' ',per.middle_name,' ',per.last_name) full_name,
 		   se.event_title, stat.status_name
 	FROM person per INNER JOIN person_event_invitation pei
@@ -24,6 +28,11 @@ END;$$
 -- Point b
 CREATE PROCEDURE getFinishedEvents(IN filter_start_date VARCHAR(11), IN filter_end_date VARCHAR(11))
 BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error executing query';
+    END;
 	DECLARE vStartDate DATE DEFAULT NULL;
     DECLARE vEndDate DATE DEFAULT NULL;
     SET vStartDate := STR_TO_DATE(filter_start_date, '%d/%m/%Y');
@@ -37,6 +46,11 @@ END;$$
 
 CREATE PROCEDURE getPendingEvents(IN filter_start_date VARCHAR(11), IN filter_end_date VARCHAR(11))
 BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error executing query';
+    END;
 	DECLARE vStartDate DATE DEFAULT NULL;
     DECLARE vEndDate DATE DEFAULT NULL;
     SET vStartDate := STR_TO_DATE(filter_start_date, '%d/%m/%Y');
@@ -47,6 +61,90 @@ BEGIN
     WHERE event_id NOT IN (
 		SELECT event_id FROM event_log
     )
-    AND se.start_date <= IFNULL(vStartDate, se.start_date)
-    AND se.end_date >= IFNULL(vEndDate, se.end_date);
+    AND start_date <= IFNULL(vStartDate, start_date)
+    AND end_date >= IFNULL(vEndDate, end_date);
+END;$$
+
+-- Point c
+CREATE PROCEDURE getEventReviews(IN filter_event_id int)
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error executing query';
+    END;
+	SELECT se.event_id, se.event_title,
+		   CONCAT(per.first_name, ' ', per.middle_name, ' ', per.last_name) person_name,
+           rev.rating, rev.review_comment
+	FROM social_event se INNER JOIN person_review rev
+    ON se.event_id = rev.event_id
+    INNER JOIN person per
+    ON per.person_id = rev.person_id
+    WHERE se.event_id = IFNULL(filter_event_id, se.event_id)
+    ORDER BY se.event_title;
+END;$$
+
+/* USERS */
+-- Point a
+CREATE PROCEDURE getInvitations(IN p_person_id int, IN filter_category_id int)
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error executing query';
+    END;
+	SELECT se.event_title, se.start_date, se.end_date, cat.category_name
+    FROM social_event se INNER JOIN person_event_invitation pei
+    ON se.event_id = pei.event_id
+    INNER JOIN category cat
+    ON cat.category_id = se.category_id
+    WHERE pei.person_id = p_person_id
+      AND cat.category_id = IFNULL(filter_category_id, cat.category_id)
+    ORDER BY se.event_title;
+END;$$
+
+-- Point b
+CREATE PROCEDURE getPublicEvents(IN p_current_user_id int)
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error executing query';
+    END;
+	SELECT se.event_title, se.start_date, se.end_date, se.photo, cat.category_name, pro.province_name,  can.canton_name, cxp.cat_id
+    FROM social_event se INNER JOIN category cat
+    ON se.category_id = cat.category_id
+    LEFT OUTER JOIN (SELECT category_id cat_id
+				FROM category_x_person
+                WHERE person_id = p_current_user_id
+			   ) cxp
+	ON cat.category_id = cxp.cat_id
+    INNER JOIN address addr
+    ON se.address_id = addr.address_id
+    INNER JOIN district dis
+    ON addr.district_id = dis.district_id
+    INNER JOIN canton can
+    ON dis.canton_id = can.canton_id
+    INNER JOIN province pro
+    ON can.province_id = pro.province_id
+    ORDER BY -cxp.cat_id DESC, se.category_id, se.event_title ASC;
+END;$$
+
+-- Point c
+CREATE PROCEDURE getAssistedEvents(IN p_current_person_id int)
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+        SELECT 'Error executing query';
+    END;
+	SELECT se.event_title, se.start_date, rev.rating, rev.review_comment
+    FROM social_event se INNER JOIN event_log el
+    ON se.event_id = el.event_id
+    INNER JOIN person_event_status pes
+    ON se.event_id = pes.event_id
+    LEFT OUTER JOIN person_review rev
+    ON se.event_id = rev.event_id
+    WHERE pes.status_type_id = 2 -- AQUI VA EL PARAMETRO
+      AND pes.person_id = p_current_person_id;
 END;$$
