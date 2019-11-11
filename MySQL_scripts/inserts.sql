d0ea5fe5346b275d567f4effa1c1e282975c9064
tentz2210 <tentz2210@hotmail.com>
2019-11-10 18:49:47 -0600

    added catalogueInserts

---
+++ MySQL_scripts/inserts.sql
@@ -0,0 +1,193 @@
+/* INSERTS COUNTRY */
+call createCountry('Costa Rica');
+call createCountry('Nicaragua');
+call createCountry('Panamá');
+call createCountry('El Salvador');
+call createCountry('Guatemala');
+
+/* INSERTS PROVINCE */
+call createProvince('San José', 1);
+call createProvince('Cartago', 1);
+call createProvince('Heredia', 1);
+call createProvince('Alajuela', 1);
+call createProvince('Limón', 1);
+call createProvince('Puntarenas', 1);
+call createProvince('Guanacaste', 1);
+call createProvince('Managua', 2);
+call createProvince('León', 2);
+call createProvince('Veraguas', 3);
+call createProvince('Los Santos', 3);
+call createProvince('La Paz', 4);
+call createProvince('Ahuachapán', 4);
+call createProvince('Puerto Barrios', 5);
+call createProvince('Alta Verapaz', 5);
+
+/* INSERTS CANTON */
+call createCanton('Central', 1);
+call createCanton('Acosta', 1);
+call createCanton('Alajuelita', 1);
+call createCanton('Aserrí', 1);
+call createCanton('Curridabat', 1);
+call createCanton('Desamparados', 1);
+call createCanton('Dota', 1);
+call createCanton('Escazú', 1);
+call createCanton('Goicochea', 1);
+call createCanton('León Cortés', 1);
+call createCanton('Montes de Oca', 1);
+call createCanton('Mora', 1);
+call createCanton('Moravia', 1);
+call createCanton('Pérez Zeledón', 1);
+call createCanton('Puriscal', 1);
+call createCanton('Santa Ana', 1);
+call createCanton('Tarrazú', 1);
+call createCanton('Tibás', 1);
+call createCanton('Turrubares', 1);
+call createCanton('Vázquez de Coronado', 1);
+
+/* INSERTS DISTRICT */
+call createDistrict('Carmen', 1);
+call createDistrict('Catedral', 1);
+call createDistrict('Hatillo', 1);
+call createDistrict('Hospital', 1);
+call createDistrict('La Uruca', 1);
+call createDistrict('Mata Redonda', 1);
+call createDistrict('Merced', 1);
+call createDistrict('Pavas', 1);
+call createDistrict('San Francisco', 1);
+call createDistrict('San Sebastián', 1);
+call createDistrict('Zapote', 1);
+    
+call createDistrict('Cangrejal', 2);
+call createDistrict('Guaitil', 2);
+call createDistrict('Palmichal', 2);
+call createDistrict('Sabanillas', 2);
+call createDistrict('San Ignacio', 2);
+    
+call createDistrict('Alajuelita', 3);
+call createDistrict('Concepción', 3);
+call createDistrict('San Antonio', 3);
+call createDistrict('San Felipe', 3);
+call createDistrict('San Josecito', 3);
+    
+call createDistrict('Aserrí', 4);
+call createDistrict('Legua', 4);
+call createDistrict('Monterrey', 4);
+call createDistrict('Salitrillos', 4);
+call createDistrict('San Gabriel', 4);
+call createDistrict('Tarbaca', 4);
+call createDistrict('Vuelta de Jorco', 4);
+    
+call createDistrict('Curridabat', 5);
+call createDistrict('Granadilla', 5);
+call createDistrict('Sánchez', 5);
+call createDistrict('Tirrases', 5);
+    
+call createDistrict('Damas', 6);
+call createDistrict('Desamparados', 6);
+call createDistrict('Frailes', 6);
+call createDistrict('Gravilias', 6);
+call createDistrict('Los Guido', 6);
+call createDistrict('Patarrá', 6);
+call createDistrict('Rosario', 6);
+call createDistrict('San Antonio', 6);
+call createDistrict('San Cristóbal', 6);
+call createDistrict('San Juan de Dios', 6);
+call createDistrict('San Miguel', 6);
+call createDistrict('San Rafael Abajo', 6);
+call createDistrict('San Rafael Arriba', 6);
+    
+call createDistrict('Copey', 7);
+call createDistrict('Jardín', 7);
+call createDistrict('Santa María', 7);
+    
+call createDistrict('Escazú', 8);
+call createDistrict('San Antonio', 8);
+call createDistrict('San Rafael', 8);
+    
+call createDistrict('Calle Blancos', 9);
+call createDistrict('Guadalupe', 9);
+call createDistrict('Ipís', 9);
+call createDistrict('Mata de Plátano', 9);
+call createDistrict('Purral', 9);
+call createDistrict('Rancho Redondo', 9);
+call createDistrict('San Francisco', 9);
+    
+call createDistrict('San Pablo', 10);
+call createDistrict('San Andrés', 10);
+call createDistrict('Llano Bonito', 10);
+call createDistrict('San Isidro', 10);
+call createDistrict('Santa Cruz', 10);
+call createDistrict('San Antonio', 10);
+  
+call createDistrict('Mercedes', 11);
+call createDistrict('Sabanilla', 11);
+call createDistrict('San Pedro', 11);
+call createDistrict('San Rafael', 11);
+   
+call createDistrict('Ciudad Colón', 12);
+call createDistrict('Guayabo', 12);
+call createDistrict('Jaris', 12);
+call createDistrict('Picagres', 12);
+call createDistrict('Piedras Negras', 12);
+call createDistrict('Tabarcia', 12);
+    
+call createDistrict('San Jerónimo', 13);
+call createDistrict('San Vicente', 13);
+call createDistrict('Trinidad', 13);
+  
+call createDistrict('San Isidro de El General', 14);
+call createDistrict('Barú', 14);
+call createDistrict('Cajón', 14);
+call createDistrict('Daniel Flores', 14);
+call createDistrict('El General', 14);
+call createDistrict('La Amistad', 14);
+call createDistrict('Páramo', 14);
+call createDistrict('Pejibaye', 14);
+call createDistrict('Platanares', 14);
+call createDistrict('Río Nuevo', 14);
+call createDistrict('Rivas', 14);
+call createDistrict('San Pedro', 14);
+  
+call createDistrict('Barbacoas', 15);
+call createDistrict('Candelarita', 15);
+call createDistrict('Chires', 15);
+call createDistrict('Desamparaditos', 15);
+call createDistrict('Grifo Alto', 15);
+call createDistrict('Mercedes Sur', 15);
+call createDistrict('San Antonio', 15);
+call createDistrict('San Rafael', 15);
+call createDistrict('Santiago', 15);
+  
+call createDistrict('Brasil', 16);
+call createDistrict('Piedades', 16);
+call createDistrict('Pozos', 16);
+call createDistrict('Salitral', 16);
+call createDistrict('Santa Ana', 16);
+call createDistrict('Uruca', 16);
+    
+call createDistrict('San Carlos', 17);
+call createDistrict('San Lorenzo', 17);
+call createDistrict('San Marcos', 17);
+  
+call createDistrict('Anselmo Llorente', 18);
+call createDistrict('Cinco Esquinas', 18);
+call createDistrict('Colima', 18);
+call createDistrict('León XIII', 18);
+call createDistrict('San Juan', 18);
+    
+call createDistrict('Carara', 19);
+call createDistrict('San Juan de Mata', 19);
+call createDistrict('San Luis', 19);
+call createDistrict('San Pablo', 19);
+call createDistrict('San Pedro', 19);
+    
+call createDistrict('Cascajal', 20);
+call createDistrict('Dulce Nombre de Jesús', 20);
+call createDistrict('Patalillo', 20);
+call createDistrict('San Isidro', 20);
+call createDistrict('San Rafael', 20);
+
+/* INSERTS USER TYPE */
+call createusertype('Administrador');
+call createusertype('Usuario');
+
